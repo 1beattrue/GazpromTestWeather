66fae1573214451c164aaae2e3ab32d8463d6b3d
Vitaly <onebeattrue@gmail.com>
2024-06-14 19:36:34 +0300

    Добавил необходимые зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.ksp)
     alias(libs.plugins.serialization)
 }
@@ -62,9 +63,6 @@ dependencies {
 
     implementation(libs.dagger.core)
     ksp(libs.dagger.compiler)
-
-    implementation(libs.room.core)
-    ksp(libs.room.compiler)
 
     implementation(libs.decompose.core)
     implementation(libs.decompose.jetpack)
